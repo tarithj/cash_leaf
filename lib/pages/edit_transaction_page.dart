cc646362a5bea48ebe2783bc9194cc9b76de4b27
Tarith Jayasooriya <tarithj@gmail.com>
2022-04-06 21:07:45 +0530

    changed edit_transaction_page to handle editors record state

--- lib/pages/edit_transaction_page.dart
+++ lib/pages/edit_transaction_page.dart
@@ -2,32 +2,49 @@ import 'package:cash_leaf/components/transaction_editor.dart';
 import 'package:cash_leaf/storage/account/record.dart';
 import 'package:flutter/material.dart';
 
-class EditTransactionPage extends StatelessWidget {
-  const EditTransactionPage({Key? key, required this.record, required this.onDone}) : super(key: key);
+class EditTransactionPage extends StatefulWidget {
+  const EditTransactionPage(
+      {Key? key, required this.record, required this.onDone})
+      : super(key: key);
 
   final Record record;
   final void Function(Record) onDone;
 
   @override
+  State<EditTransactionPage> createState() => _EditTransactionPageState();
+}
+
+class _EditTransactionPageState extends State<EditTransactionPage> {
+  late Record stateRecord;
+
+  @override
   Widget build(BuildContext context) {
+    stateRecord = widget.record;
     return Scaffold(
         appBar: AppBar(
           title: const Text("Edit Transactions"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  widget.onDone(stateRecord);
+                },
+                icon: const Icon(Icons.save))
+          ],
         ),
-        body: getBody(context)
-    );
+        body: getBody(context));
   }
 
   Widget getBody(BuildContext context) {
     return SingleChildScrollView(
-        child: Column(
-            children: [
-              Container(
-                  padding: const EdgeInsets.only(top: 10, left: 2, right: 2),
-                  child: TransactionEditor(submitButtonText: "Update", srcRecord: record, onDone: onDone)
-              )
-            ]
-        )
-    );
+        child: Column(children: [
+      Container(
+          padding: const EdgeInsets.only(top: 10, left: 2, right: 2),
+          child: TransactionEditor(
+            srcRecord: widget.record,
+            onChange: (Record r) {
+              stateRecord = r;
+            },
+          )),
+    ]));
   }
 }
